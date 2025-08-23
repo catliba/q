c461282497571d9021379aca5e98ccd2ebf8c336
Shawn Li <1caleblili@gmail.com>
2025-08-22 22:47:30 -0700

    binary search

---
+++ sqrt.py
@@ -0,0 +1,18 @@
+import math
+
+def mySqrt(self, x: int) -> int:
+    if x < 2:
+      return x
+
+    start = 1
+    end = x
+    
+    while start <= end:
+      mid = (start + end) // 2
+      if (mid * mid) == x:
+        return mid
+      elif (mid * mid) > x:
+        end = mid -1
+      else: 
+        start = mid +1
+    return end
