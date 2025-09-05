1f23608bae589dcc7c7aab07116a90d42380409b
Shawn Li <1caleblili@gmail.com>
2025-09-05 16:06:19 -0700

    not that bad actually

--- quad-sum-to-target.py
+++ quad-sum-to-target.py
@@ -1,4 +1,23 @@
 def searchQuadruplets(self, arr, target):
     quadruplets = []
-    
+    sorted_arr = sorted(arr)
+    for i in range(len(arr)):
+      if i > 0 and sorted_arr[i] == sorted_arr[i-1]:
+        continue
+     # perform searchTriplet
+      for j in range(i+1, len(arr)):
+        start = j+1
+        end = len(arr) - 1
+        while start < end:
+          target_sum = sorted_arr[i] + sorted_arr[j] + sorted_arr[start] + sorted_arr[end]
+          if target_sum == target:
+            answer_element = sorted([sorted_arr[i], sorted_arr[j], sorted_arr[start], sorted_arr[end]])
+            if answer_element not in quadruplets:
+              quadruplets.append(answer_element)
+            start +=1 
+            end -= 1
+          elif target_sum < target:
+            start += 1
+          else:
+            end -= 1
     return quadruplets
