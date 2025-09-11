8e3d531dede4114462cd858bebf45bb60ce9bd1f
Shawn Li <1caleblili@gmail.com>
2025-09-11 16:51:40 -0700

    recursive reverse

--- reverse-linked-list.py
+++ reverse-linked-list.py
@@ -14,6 +14,21 @@ def reverseList(self, head):
             curr = temp
         return prev
 
+# recursive
+def reverseList(self, head):
+    # base case: empty list or single node
+    if head is None or head.next is None:
+        return head
+    
+    # reverse the rest of the list
+    new_head = self.reverseList(head.next)
+    
+    # flip the link
+    head.next.next = head
+    head.next = None
+    
+    return new_head
+
 # errors i did: 
 # while curr != None and curr.next != None
 # return curr
