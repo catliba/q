718701940ecebd0a7a5b0927d19d7791d3a5284f
Shawn Li <1caleblili@gmail.com>
2025-09-26 22:58:47 -0700

    find smallest subarray witha greater sum

--- sliding-window/find-smallest-subarray-with-greater-sum.py
+++ sliding-window/find-smallest-subarray-with-greater-sum.py
@@ -1,3 +1,11 @@
+import math
 def findMinSubArray(self, s, arr):
-    
-    return 0   
+    window_sum, window_start = 0,0
+    min_length = math.inf
+    for window_end in range(len(arr)):
+        window_sum += arr[window_end]
+        while s <= window_sum:
+            min_length = min(min_length, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+    return 0 if min_length == float else min_length
