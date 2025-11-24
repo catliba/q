174b0d0814aeb4db869b870be58efda66edb48c0
Shawn Li <1caleblili@gmail.com>
2025-11-23 18:46:35 -0800

    finished reverse linked list

--- reverse-linked-list/every-k-element-sublist.py
+++ reverse-linked-list/every-k-element-sublist.py
@@ -33,7 +33,7 @@ class Solution:
     def reverse(self, head):
         if head is None or head.next is None:
             return head
-        new_head = self.reverseList(head.next)
+        new_head = self.reverse(head.next)
         head.next.next = head
         head.next = None
         return new_head
--- reverse-linked-list/reverse-sublist.py
+++ reverse-linked-list/reverse-sublist.py
@@ -1,5 +1,38 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.val = value
+        self.next = next
+
 class Solution:
     def reverse_sub_list(self, head, p, q):
+        start = Node(0)
+        start.next = head
+        prev = start
+        q_start = start
+        p_start = start
+
+        for _ in range(p - 1):
+            prev = prev.next
+        p_start = prev.next
+
+        for _ in range(q):
+            q_start = q_start.next
+
+        rest = q_start.next
+        q_start.next = None
+
+        # reverse linked list from p to q
+        sublist_head = self.reverse_linked_list(p_start)
+
+        p_start.next = rest
+        prev.next = sublist_head
+        return start.next
+
         return head
     def reverse_linked_list(self, head):
-        return head
+        if head is None or head.next is None: 
+            return head
+        new_head = self.reverse_linked_list(head.next) 
+        head.next.next = head 
+        head.next = None 
+        return new_head
