484f223372079359546168c68f90d0cf9170f1b9
Shawn Li <1caleblili@gmail.com>
2025-12-09 23:26:13 -0800

    tree diametre

---
+++ tree-dfs/tree-diameter.py
@@ -0,0 +1,24 @@
+#class TreeNode:
+#  def __init__(self, val, left=None, right=None):
+#    self.val = val
+#    self.left = left
+#    self.right = right
+
+
+class Solution:
+
+  def findDiameter(self, root):
+    self.treeDiameter = 0;
+    diameters = []
+    self.dfs(root, diameters)
+    return max(diameters)
+  
+  def dfs(self, node, max_diameter):
+    if node is None: 
+      return 0
+    if node.left is None and node.right is None:
+      return 1
+    L = self.dfs(node.left, max_diameter)
+    R = self.dfs(node.right, max_diameter)
+    max_diameter.append(L+R+1)
+    return 1 + max(L, R)
