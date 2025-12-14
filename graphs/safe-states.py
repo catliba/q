510abdc366f68df6eaac82f17f9c4ae6728aa9bb
Shawn Li <1caleblili@gmail.com>
2025-12-13 20:29:36 -0800

    bro this is hard

---
+++ graphs/safe-states.py
@@ -0,0 +1,8 @@
+class Solution:
+    def eventualSafeNodes(self, graph):
+        result = []
+        visited = [0] * len(graph)
+        
+        return result
+    
+    def dfs(self, node, visited):
