42f2619b3ad0529a50becd83f41019f95b5d752b
Shawn Li <1caleblili@gmail.com>
2026-01-08 23:14:31 -0800

    hard graph problem

--- graphs/safe-states.py
+++ graphs/safe-states.py
@@ -1,10 +1,24 @@
 class Solution:
     def eventualSafeNodes(self, graph):
+        def dfs(self, node, visited):
+            if visited[node] == -1:
+                return True
+            if visited[node] == 1:
+                return False
+        
+            visited[node] == 1
+            for next_node in graph[node]:
+                if not dfs(next_node):
+                    return False
+            visited[node] = -1
+            return True
         result = []
         visited = [0] * len(graph)
         
-        return result
+        for i in range(n):
+            if dfs(i):
+                result.append(i)
+        
+        return sorted(result)
     
-    def dfs(self, node, visited):
-        if visited[node] == 1:
-            return True
+    
